f26de2d2b35a697162d193fdebb5fe9c72323b33
Nadia Salmen <nadiasalmen@gmail.com>
2021-10-09 21:08:04 +0200

    Add how it works section

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from waiste.layout_params import background_image, title
+from waiste.layout_params import background_image, title, title1, title2, title3, subtitle1, subtitle2, subtitle3
 
 # Site configuration
 st.set_page_config(page_title="wAIste",
@@ -9,25 +9,55 @@ st.set_page_config(page_title="wAIste",
                    layout="centered",
                    initial_sidebar_state="collapsed")
 
-st.markdown(f'<style>{background_image}</style>', unsafe_allow_html=True)
-
-# # Sidebar with project info
-# about = st.sidebar.header('About')
-
 # Main section
 st.markdown(title, unsafe_allow_html=True)
+st.markdown(f'<style>{background_image}</style>', unsafe_allow_html=True)
 
-'How it works section!'
+# How it works section
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.markdown(title1, unsafe_allow_html=True)
+    st.markdown(subtitle1, unsafe_allow_html=True)
+    st.image(
+      "https://cdn.pixabay.com/photo/2017/07/11/10/43/upload-2493114_1280.png"
+    )
+
+with col2:
+    st.markdown(title2, unsafe_allow_html=True)
+    st.markdown(subtitle2, unsafe_allow_html=True)
+    st.image(
+        "https://cdn.pixabay.com/photo/2016/10/10/01/49/delete-1727486_1280.png"
+    )
+
+with col3:
+    st.markdown(title3, unsafe_allow_html=True)
+    st.markdown(subtitle3, unsafe_allow_html=True)
+    st.image(
+        "https://cdn.pixabay.com/photo/2016/10/08/18/35/the-location-of-the-1724293_1280.png"
+    )
+
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
 # User inputs
-name = st.text_input('Nombre:', '')
-address = st.text_input('Address:', '')
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
+name = st.text_input('Name', '')
+address = st.text_input('Address', '')
 st.subheader('Upload a photo')
 
 # User uploads image
 with st.expander("Upload your waste image..."):
     uploaded_file = st.file_uploader("Choose an image...",
                                      type=['png', 'jpg', 'jpeg'])
+
+# Acá va la predicción
+
 
 # Show map with locations
 def get_map_data():
--- waiste/layout_params.py
+++ waiste/layout_params.py
@@ -1,11 +1,20 @@
 # Background image
 background_image = """
 .stApp {
-  background-image: url(https://cdn.pixabay.com/photo/2014/01/31/06/42/glass-255281_1280.jpg);
+  background-image: linear-gradient(rgba(245,245,245,0.4),rgba(245,245,245,0.9)), url(https://cdn.pixabay.com/photo/2014/01/31/06/42/glass-255281_1280.jpg);
   background-size: cover;
-  opacity: 0.65;
-  padding: 16px;
+  padding: 8px;
 }
 """
 
-title = '<h1 style="font-family: courier; color:#FF9C2A; font-size: 64px; text-align: center;">w<span style="color:#3DFF4F;">AI</span>ste</h1>'
+# Title
+title = '<h1 style="font-family: courier; color:rgb(203 58 13); font-size: 104px; text-align: center;">w<span style="color:rgb(71 152 55);">AI</span>ste</h1>'
+
+# How it works
+title1 = '<h2 style="font-family: courier; color:rgb(71 152 55); font-size: 64px; text-align: center;">1</h2>'
+title2 = '<h2 style="font-family: courier; color:rgb(71 152 55); font-size: 64px; text-align: center;">2</h2>'
+title3 = '<h2 style="font-family: courier; color:rgb(71 152 55); font-size: 64px; text-align: center;">3</h2>'
+
+subtitle1 = '<h2 style="font-family: courier; color:rgb(203 58 13); font-size: 32px; text-align: center; padding-bottom: 40px;">Upload <span style="color:rgb(71 152 55);">image</span> of your waste</h2>'
+subtitle2 = '<h2 style="font-family: courier; color:rgb(203 58 13); font-size: 32px; text-align: center; padding-bottom: 40px;">Get your waste <span style="color:rgb(71 152 55);">category</span></h2>'
+subtitle3 = '<h2 style="font-family: courier; color:rgb(203 58 13); font-size: 32px; text-align: center; padding-bottom: 40px;">Find the <span style="color:rgb(71 152 55);">closest</span> location</h2>'
