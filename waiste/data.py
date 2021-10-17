34f5a8c0c4e36f23e88c2b895e7026b7a0583da4
Nadia Salmen <nadiasalmen@gmail.com>
2021-10-17 19:05:54 +0200

    remove tensorflow dependencies

--- waiste/data.py
+++ waiste/data.py
@@ -1,31 +1,31 @@
-from tensorflow.keras.utils import image_dataset_from_directory
+# from tensorflow.keras.utils import image_dataset_from_directory
 
-from waiste.params import IMAGE_WIDTH, IMAGE_HEIGHT, BATCH_SIZE
+# from waiste.params import IMAGE_WIDTH, IMAGE_HEIGHT, BATCH_SIZE
 
 
-def get_data():
-    filepath = "/Users/cristobalmorano/code/nadiasalmen/waiste/waiste/data"
-    data = image_dataset_from_directory(filepath,
-                                        labels='inferred',
-                                        label_mode='categorical',
-                                        class_names=None,
-                                        color_mode='rgb',
-                                        batch_size=BATCH_SIZE,
-                                        image_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-                                        shuffle=True,
-                                        seed=None,
-                                        validation_split=None,
-                                        subset=None,
-                                        interpolation='bilinear',
-                                        follow_links=False,
-                                        crop_to_aspect_ratio=False)
-    return data
+# def get_data():
+#     filepath = "/Users/cristobalmorano/code/nadiasalmen/waiste/waiste/data"
+#     data = image_dataset_from_directory(filepath,
+#                                         labels='inferred',
+#                                         label_mode='categorical',
+#                                         class_names=None,
+#                                         color_mode='rgb',
+#                                         batch_size=BATCH_SIZE,
+#                                         image_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+#                                         shuffle=True,
+#                                         seed=None,
+#                                         validation_split=None,
+#                                         subset=None,
+#                                         interpolation='bilinear',
+#                                         follow_links=False,
+#                                         crop_to_aspect_ratio=False)
+#     return data
 
-def get_categories():
-    data = get_data()
-    categories = data.class_names
-    print(categories)
-    return categories
+# def get_categories():
+#     data = get_data()
+#     categories = data.class_names
+#     print(categories)
+#     return categories
 
-if __name__ == '__main__':
-    get_categories()
+# if __name__ == '__main__':
+#     get_categories()
--- waiste/preprocessor.py
+++ waiste/preprocessor.py
@@ -1,16 +1,15 @@
-import tensorflow as tf
+# import tensorflow as tf
 
-def build_data_arrays(data):
-    data_image = []
-    data_label = []
+# def build_data_arrays(data):
+#     data_image = []
+#     data_label = []
 
-    for image, label in data:
-      data_image.append(image)
-      data_label.append(label)
+#     for image, label in data:
+#       data_image.append(image)
+#       data_label.append(label)
 
-    return data_image, data_label
+#     return data_image, data_label
 
-def cast_image_size(image):
-    image = tf.cast(image/255, tf.float32)
-    return image
-
+# def cast_image_size(image):
+#     image = tf.cast(image/255, tf.float32)
+#     return image
